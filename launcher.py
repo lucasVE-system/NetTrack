a4bd40b05e84d7446f61557e97d7eafca3ef64d9
lucasVE-system <vaneetveldelucas@gmail.com>
2026-03-23 21:53:16 +0100

    Initial commit NetTrack

--- launcher.py
+++ launcher.py
@@ -1,12 +1,15 @@
+import multiprocessing
+multiprocessing.freeze_support()
+
 import threading
 import webbrowser
 import time
 import app
 
 def open_browser():
-    time.sleep(1.5)
+    time.sleep(2)
     webbrowser.open("http://127.0.0.1:5000")
 
 if __name__ == "__main__":
-    threading.Thread(target=open_browser).start()
-    app.app.run(host="0.0.0.0", port=5000, debug=False)
+    threading.Thread(target=open_browser, daemon=True).start()
+    app.app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False, threaded=True)
