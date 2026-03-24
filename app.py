fcd83e88873d787fdfb0e899b84406cef3358661
lucasVE-system <vaneetveldelucas@gmail.com>
2026-03-24 11:21:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -213,4 +213,4 @@ def delete_device():
     return jsonify({"ok": False, "error": "Device not found"}), 404
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="127.0.0.1", port=5000, debug=False)
